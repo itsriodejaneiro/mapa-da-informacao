f84a413dfc01addc7dc2f1ba5760a5a346f85116
marlus <mar251@gmail.com>
2018-07-16 06:21:52 -0300

    engine updates

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var viewport = svg.append("g")
 
 var manyBody = d3.forceManyBody()
 
-manyBody.strength(100)
+manyBody.strength(200)
 //manyBody.theta(0.6)
 
 var simulation = d3.forceSimulation()
@@ -24,12 +24,16 @@ var simulation = d3.forceSimulation()
 	.force("link", d3.forceLink().id(function(d) { return d.id; }))
 	.force("center", d3.forceCenter(width / 2, height / 2))
 	.force('collision', d3.forceCollide().radius(function(d) {
-		return node_size(d.tipo) * 2
+		return node_size(d) * 2.4
 	}))
 	.on("tick", ticked)
+	.alphaTarget(0.01)
+	.alphaDecay(0.1)
+	.alphaMin(0.0100000001)
 
 var tooltip = d3.select('.tooltip')
 var fx = new TextScramble(document.querySelector('.tooltip-text'),15)
+var fxto = null
 
 var graph = {}
 
@@ -56,21 +60,40 @@ d3.csv('./data/data-nodes.csv')
 
 		var _relations = d
 		var _links = []
+		var _linksori = []
 		
 		_relations.map(function(d){
 			_links.push({source: d.source, target: d.target})	
+			_linksori.push({source: d.source, target: d.target})	
 		})
 
-		var k = 0
+		var k = {
+			app: 0,
+			base: 0,
+			doc: 0,
+			orgao: 0,
+			politica: 0,
+			servico: 0
+		}
 
 		_nodes.map(function(d){
-			d.fx = null
-			d.offsetY = k
-			k += 15
-			k = k % 75
+			// offset
+			var step = d.tipo == 'base' ? 50 : 25
+			var loop = d.tipo == 'base' ? 200 : 125
+			d.offsetY = k[d.tipo]
+			k[d.tipo] += step
+			k[d.tipo] = k[d.tipo] % loop
+			// radius
+			if(d.tipo == 'base'){
+				var arr = _.filter(_linksori, function(o) { return o.source  == d.id })
+				d.links = arr.length
+			} else {
+				var arr = _.filter(_linksori, function(o) { return o.target  == d.id })
+				d.links = arr.length
+			}
 		})
 
-		console.log(_nodes)
+		//console.log(_nodes)
 
 		graph.nodes = _nodes
 		graph.links = _links
@@ -81,7 +104,7 @@ d3.csv('./data/data-nodes.csv')
 
 function ticked() {
 
-	//simulation.alpha(0.08)
+	//simulation.alpha(0.1)
 
 	//nodes.filter(function(d){return d.id == 'governo'}).attr('fx',0).attr('fy',0)
 
@@ -94,7 +117,7 @@ function ticked() {
 		nodes.each(function(d, i) {
 			ky = 0.1
 			d.x -= (d.x - width / 2) * 8 * 0.0001;
-			d.y -= (d.y + d.offsetY - (node_cluster(d.tipo) + 1) * 100) * 5 * ky;
+			d.y -= (d.y + d.offsetY - (node_cluster(d.tipo) + 1) * 120) * 5 * ky;
 		})
 
 		nodes
@@ -163,6 +186,7 @@ function update(data_n,data_l){
 		.attr('node_id', function(d) {
 			return d.id;
 		})
+		//.call(drag_handler)
 		.append("circle")
 		.attr("r", 0)
 		.on("mouseover", node_mouseover)
@@ -170,7 +194,7 @@ function update(data_n,data_l){
 		.on("click", node_click)
 		.transition(t)
 		.delay(function(d, i) { return node_delay(d.tipo,i) })
-		.attr("r", function(d){ return node_size(d.tipo) } )
+		.attr("r", function(d){ return node_size(d) } )
 		.attr("fill", function(d) { return node_color(d.tipo); })
 		
 
@@ -190,7 +214,7 @@ function update(data_n,data_l){
 			.text(function(d) { return d.nome; })
 			.attr("text-anchor", "middle")
 			.attr('x', 0)
-			.attr('y', function(d){ return node_size(d.tipo) * 1.5 + 8 })
+			.attr('y', function(d){ return node_size(d) + 16 })
 			.attr("opacity", 0)
 			.transition(t)
 			.delay(function(d, i) { return node_delay(d.tipo, i + 10) })
@@ -214,7 +238,7 @@ function update(data_n,data_l){
 		.attr("opacity", 0)
 		//.attr("stroke-width", function(d) { return Math.sqrt(d.value); })
 		.transition(t)
-		.delay(6000)
+		.delay(4000)
 		.attr("opacity", 1)
 
 
@@ -224,6 +248,7 @@ function update(data_n,data_l){
 		.links(data_l)
 
 
+	//drag_handler(nodes)
 	//simulation.alpha(0.1)
 
 	console.log('UPDATE VIS', data_n, data_l)
@@ -231,69 +256,60 @@ function update(data_n,data_l){
 
 }
 
+// DRAG
+
+var drag_handler = d3.drag()
+	.on("start", drag_start)
+	.on("drag", drag_drag)
+	.on("end", drag_end)
+
+function drag_start(d) {
+	if (!d3.event.active) simulation.alphaTarget(0.1).restart();
+	d.fx = d.x;
+	d.fy = d.y;
+}
+
+function drag_drag(d) {
+	d.fx = d3.event.x;
+	d.fy = d3.event.y;
+}
+
+function drag_end(d) {
+	if (!d3.event.active) simulation.alphaTarget(0.01);
+	d.fx = null;
+	d.fy = null;
+}
+
+//*/
+
+// ZOOM
+
+var zoom_handler = d3.zoom()
+	.on("zoom", zoom_actions)
+
+function zoom_actions(){
+	viewport.attr("transform", d3.event.transform)
+}
+
+//zoom_handler(svg)
 
 
-	// DRAG
+// RESIZE
 
-	/*
+function resize() {
+	
+	width = window.innerWidth
+	height = window.innerHeight
 
-	var drag_handler = d3.drag()
-		.on("start", drag_start)
-		.on("drag", drag_drag)
-		.on("end", drag_end); 
-		
-	drag_handler(node)
+	svg.attr("width", width).attr("height", height);
 
-	function drag_start(d) {
-		if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-		d.fx = d.x;
-		d.fy = d.y;
-	}
-
-	function drag_drag(d) {
-		d.fx = d3.event.x;
-		d.fy = d3.event.y;
-	}
-
-	function drag_end(d) {
-		if (!d3.event.active) simulation.alphaTarget(0);
-		d.fx = null;
-		d.fy = null;
-	}
-
-	*/
-
-	// ZOOM
-
-	/*
-	var zoom_handler = d3.zoom()
-			.on("zoom", zoom_actions);
-
-	zoom_handler(svg);
-
-	function zoom_actions(){
-			viewport.attr("transform", d3.event.transform)
-	}
-	*/
-
-	// RESIZE
-
-	/*
-	function resize() {
-		
-		width = window.innerWidth
-		height = window.innerHeight
-
-		svg.attr("width", width).attr("height", height);
-
-		//force.size([force.size()[0]+(width-w)/zoom.scale(),force.size()[1]+(height-h)/zoom.scale()]).resume();
-	}
+	//force.size([force.size()[0]+(width-w)/zoom.scale(),force.size()[1]+(height-h)/zoom.scale()]).resume();
+}
 
 
-	d3.select(window).on("resize", resize)
-	resize()
+//d3.select(window).on("resize", resize)
+//resize()
 
-	*/
 
 // EVENTS
 
@@ -303,7 +319,7 @@ function node_click(d) {
 }
 
 function node_mouseover(d) {
-	
+
 	// label
 
 	var text = d3.select('.label[label_id="' + d.id + '"]')
@@ -325,15 +341,18 @@ function node_mouseover(d) {
 	var svg_w = d3.select('.overlay').node().getBoundingClientRect().width
 	var scale = svg_w / width
 
-	var top = d.y < height * .6
-		? d.y * scale + (node_size(d.tipo) * 0.5 + 30) * scale
-		: d.y * scale - (node_size(d.tipo) * scale + 60)
+	var top = d.y < height * .8
+		? d.y * scale + (node_size(d) * 0.5 + 30) * scale
+		: d.y * scale - (node_size(d) * scale + 70)
 
 	var left = d.x < width * .75
 		? (d.x + 20) * scale
 		: (d.x - 20) * scale
 
-	console.log(svg_w, scale, top, left)
+	// window safe area
+	left = Math.min(Math.max(180,left),width * scale - 180)
+
+	//console.log(svg_w, scale, top, left)
 
 	d3.selectAll('.tooltip-title')
 		.text(tipo_label(d.tipo))
@@ -346,7 +365,13 @@ function node_mouseover(d) {
 		.duration(100)
 		.style('left', left + 'px')
 		
-	fx.setText(d.nome)
+	fx.setText(d.nomecompleto || d.nome)
+
+	// if(d.nomecompleto){
+	// 	fxto = setTimeout(function(){
+	// 		fx.setText(d.nomecompleto)
+	// 	},1000)
+	// }
 
 	sound_over.play()
 
@@ -372,6 +397,8 @@ function node_mouseout(d) {
 
 	// tooltip
 
+	//clearTimeout(fxto)
+	fx.setText(d.nome)
 	tooltip.classed('show', false)	
 }
 
@@ -380,17 +407,18 @@ function node_mouseout(d) {
 var color = d3.scaleOrdinal(d3.schemeCategory20)
 
 var templates = {
-	doc      : { size: 20, cluster_y: 0, delay: 0, color: 1, r: 100 * 1 },
-	app      : { size: 10, cluster_y: 1, delay: 5, color: 2, r: 100 * 7 },
-	base     : { size: 30, cluster_y: 2, delay: 1, color: 3, r: 100 * 2 },
-	orgao    : { size: 15, cluster_y: 4, delay: 2, color: 4, r: 100 * 4 },
-	servico  : { size: 10, cluster_y: 5, delay: 3, color: 5, r: 100 * 5 },
-	politica : { size: 10, cluster_y: 6, delay: 4, color: 6, r: 100 * 6 },
+	doc      : { size: 15, cluster_y: 1.0, delay: 0, color: 1 },
+	app      : { size:  5, cluster_y: 2.3, delay: 5, color: 2 },
+	base     : { size: 20, cluster_y: 4.0, delay: 1, color: 3 },
+	orgao    : { size: 10, cluster_y: 6.0, delay: 2, color: 4 },
+	servico  : { size:  5, cluster_y: 7.0, delay: 3, color: 5 },
+	politica : { size:  5, cluster_y: 7.5, delay: 4, color: 6 },
 }
 
 
-function node_size(t){
-	return templates[t].size
+function node_size(d){
+	var extra = d.tipo == 'base' ? d.links : d.links * 2
+	return templates[d.tipo].size + extra
 }
 
 function node_color(t){
@@ -402,7 +430,7 @@ function node_cluster(t){
 }
 
 function node_delay(t,i){
-	return templates[t].delay * 300 + i * 100
+	return templates[t].delay * 100 + i * 50
 }
 
 function tipo_label(t){
