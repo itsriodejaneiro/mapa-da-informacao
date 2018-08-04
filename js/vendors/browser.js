354e37c35c62c257c0bcff824f21e83e7d4cd499
marlus <mar251@gmail.com>
2018-08-04 12:14:02 -0300

    historias no mobile

--- js/vendors/browser.js
+++ js/vendors/browser.js
@@ -1,83 +1,9 @@
-// contain and invoke browser API
-(function() {
+jQuery(document).ready(function($){
+var deviceAgent = navigator.userAgent.toLowerCase();
+var agentID = deviceAgent.match(/(iphone|ipod|ipad)/);
+    if (agentID) {
+ 
+	$('html').addClass('ios');
 
-    var matched, browser;
-
-// More details: https://api.jquery.com/jQuery.browser
-// jQuery.uaMatch maintained for back-compat
-    jQuery.uaMatch = function( ua ) {
-        ua = ua.toLowerCase();
-
-        var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
-            /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
-            /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
-            /(msie) ([\w.]+)/.exec( ua ) ||
-            ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
-            [];
-
-        return {
-            browser: match[ 1 ] || "",
-            version: match[ 2 ] || "0"
-        };
-    };
-
-    matched = jQuery.uaMatch( navigator.userAgent );
-    browser = {};
-
-    if ( matched.browser ) {
-        browser[ matched.browser ] = true;
-        browser.version = matched.version;
     }
-
-// Chrome is Webkit, but Webkit is also Safari.
-    if ( browser.chrome ) {
-        browser.webkit = true;
-    } else if ( browser.webkit ) {
-        browser.safari = true;
-    }
-
-    jQuery.browser = browser;
-
-    jQuery.sub = function() {
-        function jQuerySub( selector, context ) {
-            return new jQuerySub.fn.init( selector, context );
-        }
-        jQuery.extend( true, jQuerySub, this );
-        jQuerySub.superclass = this;
-        jQuerySub.fn = jQuerySub.prototype = this();
-        jQuerySub.fn.constructor = jQuerySub;
-        jQuerySub.sub = this.sub;
-        jQuerySub.fn.init = function init( selector, context ) {
-            if ( context && context instanceof jQuery && !(context instanceof jQuerySub) ) {
-                context = jQuerySub( context );
-            }
-
-            return jQuery.fn.init.call( this, selector, context, rootjQuerySub );
-        };
-        jQuerySub.fn.init.prototype = jQuerySub.fn;
-        var rootjQuerySub = jQuerySub(document);
-        return jQuerySub;
-    };
-
-})();
-$(document).ready(function () {
-    // Get browser and add to body
-    $.each($.browser, function (i) {
-        $('body').addClass(i);
-        return false;
-    });
-    // Get OS and add to body
-    var os = [
-        'iphone',
-        'ipad',
-        'windows',
-        'mac',
-        'linux'
-    ];
-    var match = navigator.appVersion.toLowerCase().match(new RegExp(os.join('|')));
-    if (match) {
-        $('body').addClass(match[0]);
-    };
-  //https://codepen.io/freshmasterj/pen/NNzmdj
-  //$('body').html($('body').attr('class'));
 });
