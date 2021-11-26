cc882e86ec024a5a68f87ea051ca340f17e43f1f
Tainá Simões <taina.simoes.santos@gmail.com>
2021-11-26 18:16:57 -0300

    change manifest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ae8a58b79b3132be772e"; }
+/******/ 		__webpack_require__.h = function() { return "eb8de791978305e9844c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
