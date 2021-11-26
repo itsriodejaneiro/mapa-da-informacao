023f44cae3e38bf1339774da7ae96f3c5e15bd61
Tainá Simões <taina.simoes.santos@gmail.com>
2021-11-26 18:33:29 -0300

    remove cache

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eb8de791978305e9844c"; }
+/******/ 		__webpack_require__.h = function() { return "ab42c92fced16495e38c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
