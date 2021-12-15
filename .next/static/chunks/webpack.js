cb42016cd515d2e9e8ad9113b6f532a9eb628f17
Tainá Simões <taina.simoes.santos@gmail.com>
2021-12-15 12:42:54 -0300

    fix links path class

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5fd252789254b4fd2f0e"; }
+/******/ 		__webpack_require__.h = function() { return "43a765450fc62aa9db66"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
