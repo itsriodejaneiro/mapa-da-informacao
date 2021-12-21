aa53fcaff32e4f708b8f99837d232d5e2567f825
Tainá Simões <taina.simoes.santos@gmail.com>
2021-12-21 17:35:29 -0300

    change css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "af3b3a378a093dfe17d5"; }
+/******/ 		__webpack_require__.h = function() { return "32980834d7ba51cd18d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
