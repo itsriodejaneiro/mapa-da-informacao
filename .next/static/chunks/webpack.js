a82847978bf2b657f52fa4e13eca04d8e99e702f
Tainá Simões <taina.simoes.santos@gmail.com>
2022-01-27 16:38:07 -0300

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "42485c6d7434c1333798"; }
+/******/ 		__webpack_require__.h = function() { return "1af92c5761d7a1426ffb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
